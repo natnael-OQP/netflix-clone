c2a75ade4a54b33b2f920f439ea61ab822eb52a7
papa-react <makiya112545@gmail.com>
2021-10-07 23:53:08 -1200

    neflix clone  initialize

---
+++ src/database/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC0J7rMyMdAk2zizrjuf352Z52j0fmRS94",
+    authDomain: "netflix-clone-with-nathan.firebaseapp.com",
+    projectId: "netflix-clone-with-nathan",
+    storageBucket: "netflix-clone-with-nathan.appspot.com",
+    messagingSenderId: "597773272580",
+    appId: "1:597773272580:web:0c8df66cce7a7c42775751"
+};
+
+const app = initializeApp(firebaseConfig)
