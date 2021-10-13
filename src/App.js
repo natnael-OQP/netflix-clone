3df6a2a32d7fd479b565dc191e86f743c41f3feb
papa-react <makiya112545@gmail.com>
2021-10-12 17:39:11 -1200

    set up react-router dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import GlobalStyle from './globalStyle';
 import HomePage from './pages/home/HomePage';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <GlobalStyle/>
-      <HomePage/>
-    </>
+      <HomePage />
+      <Switch>
+          <Route path="/">
+            <HomePage />
+          </Route>
+          <Route path="/profile">
+            {
+              // <Users />
+            }
+        </Route>
+        </Switch>
+    </Router>
   );
 }
 
