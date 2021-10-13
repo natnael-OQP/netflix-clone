355bcad0470d3ce1d04bbedf1670242d54eec4b8
papa-react <makiya112545@gmail.com>
2021-10-12 20:22:06 -1200

    Sign up Component

---
+++ src/component/SignIn/SignIn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SignInContainer } from '.'
+
+const SignIn = () => {
+    return (
+        <SignInContainer>
+            
+        </SignInContainer>
+    )
+}
+
+export default SignIn
---
+++ src/component/SignIn/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+
+export const SignInContainer = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-40%,-50%);
+    z-index: 999;
+    width:400px;
+    height:500px;
+    background-color: rgb(0,0,0,.9);
+`;
