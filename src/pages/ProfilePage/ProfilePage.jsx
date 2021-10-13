902284ae40327a0c72a29db817c89b9069b5651b
papa-react <makiya112545@gmail.com>
2021-10-13 05:48:03 -1200

    profile page-ui part creted

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
 import {css} from 'styled-components/macro';
-import { Email, ProfileBody, ProfileContainer, ProfileDetail, ProfileImage, ProfilePlans } from '.'
-import { BtnRight } from '../../component/Button/Button'
+import { Email, Plan, PlanName, PlanNameWrapper, PlanQuality, PlansTitle, ProfileBody, ProfileContainer, ProfileDetail, ProfileImage, ProfileInfo, ProfilePlans,Time,Title } from '.'
+import ButtonR, { BtnRight } from '../../component/Button/Button'
 import { HederWrapper, Image, } from '../../component/Header';
-import { Title } from '../../component/SignIn';
+import { SelectUser } from '../../features/userSlice';
+import {auth} from '../../database/firebase'
 
 const ProfilePage = () => {
-    const history = useHistory()
+    const history = useHistory();
+    const user = useSelector(SelectUser)
     return (
         <ProfileContainer>
             <HederWrapper css={` background-color: #111; `}>
@@ -15,15 +18,46 @@ const ProfilePage = () => {
                 <BtnRight size="1rem" padding="7px 17px" bgColor="#e50914"  >Sign In</BtnRight>
             </HederWrapper>
             <ProfileBody>
-                <Title css={' color:#000; font-size: 3rem; '} >Edit Me</Title>
-                <profileInfo>
+                <Title css={' color:#fff; font-size: 3rem; '} >Edit Profile</Title>
+                <ProfileInfo>
                     <ProfileImage src='https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png' alt="profile" />
                     <ProfileDetail>
-                        <Email></Email>
+                        <Email>{user.email}</Email>
                         <ProfilePlans>
+                            <PlansTitle>plans</PlansTitle>
+                            <Time>Renewal Date:09/11/2020 </Time>
+                            <Plan>
+                                <PlanNameWrapper>
+                                    <PlanName>Etflix Standard </PlanName>
+                                    <PlanQuality>1080P</PlanQuality>
+                                </PlanNameWrapper>
+                                <ButtonR  size=".89rem" padding="7px 17px" bgColor="#e50914" css={` border-radius:3px; height:35px;`}>Subscribe</ButtonR>
+                            </Plan>
+                            <Plan>
+                                <PlanNameWrapper>
+                                    <PlanName>Etflix Basic </PlanName>
+                                    <PlanQuality>480P</PlanQuality>
+                                </PlanNameWrapper>
+                                <ButtonR  size=".89rem" padding="7px 17px" bgColor="#e50914" css={` border-radius:3px; height:35px;`}>Subscribe</ButtonR>
+                            </Plan>
+                            <Plan>
+                                <PlanNameWrapper>
+                                    <PlanName>Etflix Standard </PlanName>
+                                    <PlanQuality>4K+HDR</PlanQuality>
+                                </PlanNameWrapper>
+                                <ButtonR  size=".89rem" padding="7px 17px" bgColor="#e50914" css={` border-radius:3px; height:35px;`}>Subscribe</ButtonR>
+                            </Plan>
+                            
+                            <ButtonR  size=".89rem" padding="7px 17px" bgColor="#e50914" width="100%"
+                                css={` 
+                                        margin-top:.5rem; 
+                                        border-radius:5px;
+                                        height:35px;`}
+                                onClick={()=> auth.signOut()}
+                                >Sign Out</ButtonR>
                         </ProfilePlans>
                     </ProfileDetail>
-                </profileInfo>
+                </ProfileInfo>
             </ProfileBody>
         </ProfileContainer>
     )
--- src/pages/ProfilePage/index.js
+++ src/pages/ProfilePage/index.js
@@ -5,29 +5,91 @@ export const ProfileContainer = styled.div`
     height:100vh;
     overflow: hidden;
     position: relative;
+    color:#fff;
+    background-color: #000;
 `;
 
 export const ProfileBody = styled.div`
-    position: absolute;
-    top:30%;
+    /* position: absolute; */
+    display:flex;
+    flex-direction: column;
+    width:50%;
+    margin:0 auto;
+    max-width:700px;
+    /* background-color: #fff; */
+    margin-top:70px;
+`;
+
+export const Title = styled.h1`
+    font-size:3rem;
+    font-weight: 400;
+    font-family: 'Poppins', sans-serif;
+    color:#fff;
+    margin-bottom:2rem;
+    border-bottom: 1px solid w;
+`;
+
+export const ProfileInfo = styled.div`
+    display:flex;
 `;
 
 export const ProfileImage = styled.img`
-    
+    width:80px;
+    height:80px;
+    object-fit: contain;
 `;
 
-
-
 export const ProfileDetail = styled.div`
-    
+    display: flex;
+    flex-direction: column;
+    flex:1;
+    margin-left:1rem;
 `;
 
 export const Email = styled.h4`
-    
+    padding:.4rem 1.5rem;
+    background-color:#32333B;
+    font-weight: 400;
+    font-family: 'Poppins', sans-serif;
 `;
                         
 
 export const ProfilePlans = styled.div`
-    
+    display: flex;
+    flex-direction: column;
 `;
                         
+export const PlansTitle = styled.h3`
+    padding:.3rem;
+    border-bottom: 1px solid #282c2d;
+    font-weight: 400;
+    font-family: 'Poppins', sans-serif;
+`;
+
+export const Time = styled.h4`
+padding:.4rem;
+font-weight: 400;
+font-family: 'Poppins', sans-serif;
+    
+`;
+export const Plan = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding:1rem 2rem;
+`;
+
+export const PlanNameWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const PlanName = styled.h4`
+    font-weight: 400;
+    font-family: 'Poppins', sans-serif;
+`;
+
+export const PlanQuality = styled.h5`
+    font-weight: 400;
+    font-family: 'Poppins', sans-serif;
+`;
